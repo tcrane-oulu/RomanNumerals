aef59114995924c19f0e2d926085618d6dd4a57c
tcrane-oulu <thomas.crane@student.oulu.fi>
2018-10-11 15:09:34 +0300

    Assignment finished.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,103 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
-	}
+  public static int convertToInteger(String romanNum) {
+    if (romanNum.length() == 0) {
+      return 0;
+    }
+    String str = romanNum.toUpperCase();
+    int lastCharCount = 0;
+    char lastChar = '#';
+    int sum = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (lastChar != '#' && str.charAt(i) == lastChar) {
+        lastCharCount++;
+        if (repeatedTooManyTimes(lastChar, lastCharCount)) {
+          return -1;
+        }
+      } else {
+        lastCharCount = 1;
+        lastChar = str.charAt(i);
+      }
+      if (i < str.length() - 1) {
+        if (isSubtractable(str.charAt(i), str.charAt(i + 1))) {
+          lastChar = str.charAt(i + 1);
+          lastCharCount = 1;
+          sum += getValue(str.charAt(i + 1)) - getValue(str.charAt(i));
+          i++;
+          continue;
+        }
+      }
+      sum += getValue(str.charAt(i));
+    }
+    // To be Implemented
+    return sum;
+  }
+
+  private static boolean isSubtractable(char c, char nextChar) {
+    if (c != 'I' && c != 'X' && c != 'C') {
+      return false;
+    }
+    if (getPrecedence(nextChar) <= getPrecedence(c)) {
+      return false;
+    }
+    if (getPrecedence(nextChar) - 2 > getPrecedence(c)) {
+      return false;
+    }
+    return true;
+  }
+
+  private static int getValue(char c) {
+    switch (c) {
+    case 'I':
+      return 1;
+    case 'V':
+      return 5;
+    case 'X':
+      return 10;
+    case 'L':
+      return 50;
+    case 'C':
+      return 100;
+    case 'D':
+      return 500;
+    case 'M':
+      return 1000;
+    }
+    return 0;
+  }
+
+  private static int getPrecedence(char c) {
+    switch (c) {
+    case 'I':
+      return 0;
+    case 'V':
+      return 1;
+    case 'X':
+      return 2;
+    case 'L':
+      return 3;
+    case 'C':
+      return 4;
+    case 'D':
+      return 5;
+    case 'M':
+      return 6;
+    }
+    return -1;
+  }
+
+  private static boolean repeatedTooManyTimes(char c, int count) {
+    switch (c) {
+    case 'I':
+    case 'X':
+    case 'C':
+    case 'M':
+      return count > 3;
+    case 'V':
+    case 'L':
+    case 'D':
+      return count > 1;
+    }
+    return false;
+  }
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,138 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+  @Test
+  public void convertToInteger_IsCorrectForI() {
+    assertEquals(1, RomanNumerals.convertToInteger("I"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForV() {
+    assertEquals(5, RomanNumerals.convertToInteger("V"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForX() {
+    assertEquals(10, RomanNumerals.convertToInteger("X"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForL() {
+    assertEquals(50, RomanNumerals.convertToInteger("L"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForC() {
+    assertEquals(100, RomanNumerals.convertToInteger("C"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForD() {
+    assertEquals(500, RomanNumerals.convertToInteger("D"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForM() {
+    assertEquals(1000, RomanNumerals.convertToInteger("M"));
+  }
+  @Test
+  public void convertToInteger_IsCorrectForMultipleLetters() {
+    assertEquals(2, RomanNumerals.convertToInteger("II"));
+    assertEquals(3, RomanNumerals.convertToInteger("III"));
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    assertEquals(20, RomanNumerals.convertToInteger("XX"));
+    assertEquals(30, RomanNumerals.convertToInteger("XXX"));
 
+    assertEquals(200, RomanNumerals.convertToInteger("CC"));
+    assertEquals(300, RomanNumerals.convertToInteger("CCC"));
+  }
+
+  @Test
+  public void convertToInteger_IsCorrectForCombinations() {
+    assertEquals(6, RomanNumerals.convertToInteger("VI"));
+
+    assertEquals(60, RomanNumerals.convertToInteger("LX"));
+
+    assertEquals(600, RomanNumerals.convertToInteger("DC"));
+  }
+
+  @Test
+  public void convertToInteger_DoesNotAllowRepetition() {
+    assertEquals(-1, RomanNumerals.convertToInteger("IIII"));
+    assertEquals(-1, RomanNumerals.convertToInteger("XXXX"));
+    assertEquals(-1, RomanNumerals.convertToInteger("CCCC"));
+
+    assertEquals(-1, RomanNumerals.convertToInteger("VV"));
+    assertEquals(-1, RomanNumerals.convertToInteger("LL"));
+    assertEquals(-1, RomanNumerals.convertToInteger("DD"));
+  }
+
+  @Test
+  public void convertToInteger_DoesAllowSeparatedRepetition() {
+    assertEquals(9, RomanNumerals.convertToInteger("IIIVIII"));
+  }
+
+  @Test
+  public void convertToInteger_1SymbolsAreSubtractedCorrectly() {
+    assertEquals(4, RomanNumerals.convertToInteger("IV"));
+    assertEquals(9, RomanNumerals.convertToInteger("IX"));
+
+    assertEquals(40, RomanNumerals.convertToInteger("XL"));
+    assertEquals(90, RomanNumerals.convertToInteger("XC"));
+
+    assertEquals(400, RomanNumerals.convertToInteger("CD"));
+    assertEquals(900, RomanNumerals.convertToInteger("CM"));
+  }
+
+  @Test
+  public void convertToInteger_OnlyOneSubtractionAllowed() {
+    assertEquals(5, RomanNumerals.convertToInteger("IIV"));
+    assertEquals(10, RomanNumerals.convertToInteger("IIX"));
+
+    assertEquals(50, RomanNumerals.convertToInteger("XXL"));
+    assertEquals(100, RomanNumerals.convertToInteger("XXC"));
+
+    assertEquals(500, RomanNumerals.convertToInteger("CCD"));
+    assertEquals(1000, RomanNumerals.convertToInteger("CCM"));
+  }
+
+  @Test
+  public void convertToInteger_5SymbolsCannotBeSubtracted() {
+    assertEquals(15, RomanNumerals.convertToInteger("VX"));
+    assertEquals(150, RomanNumerals.convertToInteger("LC"));
+    assertEquals(1500, RomanNumerals.convertToInteger("DM"));
+  }
+
+  @Test
+  public void convertToInteger_WorksForVariousCombinations() {
+    assertEquals(1984, RomanNumerals.convertToInteger("MCMLXXXIV"));
+    assertEquals(2014, RomanNumerals.convertToInteger("MMXIV"));
+  }
+
+  @Test
+  public void convertToInteger_WorksForAllBasicCombinations() {
+    assertEquals(1, RomanNumerals.convertToInteger("I"));
+    assertEquals(2, RomanNumerals.convertToInteger("II"));
+    assertEquals(3, RomanNumerals.convertToInteger("III"));
+    assertEquals(4, RomanNumerals.convertToInteger("IV"));
+    assertEquals(5, RomanNumerals.convertToInteger("V"));
+    assertEquals(6, RomanNumerals.convertToInteger("VI"));
+    assertEquals(7, RomanNumerals.convertToInteger("VII"));
+    assertEquals(8, RomanNumerals.convertToInteger("VIII"));
+    assertEquals(9, RomanNumerals.convertToInteger("IX"));
+
+    assertEquals(10, RomanNumerals.convertToInteger("X"));
+    assertEquals(20, RomanNumerals.convertToInteger("XX"));
+    assertEquals(30, RomanNumerals.convertToInteger("XXX"));
+    assertEquals(40, RomanNumerals.convertToInteger("XL"));
+    assertEquals(50, RomanNumerals.convertToInteger("L"));
+    assertEquals(60, RomanNumerals.convertToInteger("LX"));
+    assertEquals(70, RomanNumerals.convertToInteger("LXX"));
+    assertEquals(80, RomanNumerals.convertToInteger("LXXX"));
+    assertEquals(90, RomanNumerals.convertToInteger("XC"));
+
+    assertEquals(100, RomanNumerals.convertToInteger("C"));
+    assertEquals(200, RomanNumerals.convertToInteger("CC"));
+    assertEquals(300, RomanNumerals.convertToInteger("CCC"));
+    assertEquals(400, RomanNumerals.convertToInteger("CD"));
+    assertEquals(500, RomanNumerals.convertToInteger("D"));
+    assertEquals(600, RomanNumerals.convertToInteger("DC"));
+    assertEquals(700, RomanNumerals.convertToInteger("DCC"));
+    assertEquals(800, RomanNumerals.convertToInteger("DCCC"));
+    assertEquals(900, RomanNumerals.convertToInteger("CM"));
+  }
 }
